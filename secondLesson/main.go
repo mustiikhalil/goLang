c535a44e9d7198c4f3409414464872365d880fa4
mustii <mustii@mmk.one>
2018-12-07 23:52:46 +0300

    Started second lesson

---
+++ secondLesson/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Whoa, go is neat!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "about page")
+}
+
+func main() {
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8000", nil)
+}
