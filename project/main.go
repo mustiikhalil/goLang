75c5aea3c85c46b11e381c7d1da3fc9baf55a735
mustii <mustii@mmk.one>
2018-12-13 18:11:16 +0300

    Pasing XML and fetching it from WP

--- project/main.go
+++ project/main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-	"os"
-
-	"github.com/go-http-utils/logger"
+	"strings"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1> Hello there </h1>")
+// for parsing the base index.xml
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+// Parsing the news
+type News struct {
+	Titles    []string `xml:"url>news>title"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+// site map
+type NewsMap struct {
+	Keywords string
+	Location string
 }
 
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8000", logger.Handler(router, os.Stdout, logger.DevLoggerType))
+	var s Sitemapindex
+	var n News
+
+	// hash map to save all the data
+	newsMap := make(map[string]NewsMap)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+
+	for _, Location := range s.Locations {
+		// Hack to solve a problem with https://
+		s := strings.Split(Location, "//")
+
+		resp, _ := http.Get("https://" + s[1])
+		bytes, _ := ioutil.ReadAll(resp.Body)
+
+		xml.Unmarshal(bytes, &n)
+
+		for idx, _ := range n.Titles {
+			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+	fmt.Println(newsMap)
 }
