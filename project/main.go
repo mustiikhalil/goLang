0b6e933adf87b8567a42dcb10ddd0ae6b3a726cc
mustii <mustii@mmk.one>
2018-12-16 21:32:53 +0300

    Learning Goroutines

--- project/main.go
+++ project/main.go
@@ -6,7 +6,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"text/template"
 )
+
+var newsMap = make(map[string]NewsMap)
+
+type NewsAggPage struct {
+	Title string
+	News  map[string]NewsMap
+}
 
 // for parsing the base index.xml
 type Sitemapindex struct {
@@ -26,12 +34,11 @@ type NewsMap struct {
 	Location string
 }
 
-func main() {
+func NewsAgg() {
 	var s Sitemapindex
 	var n News
 
 	// hash map to save all the data
-	newsMap := make(map[string]NewsMap)
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
@@ -49,5 +56,17 @@ func main() {
 			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
 		}
 	}
-	fmt.Println(newsMap)
+	fmt.Println("done")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Amazing news AGG", News: newsMap}
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, p)
+}
+
+func main() {
+	go NewsAgg() // concurrent go routine
+	// http.HandleFunc("/", indexHandler)
+	// http.ListenAndServe(":8000", nil)
 }
---
+++ project/sync.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 100)
+	}
+}
